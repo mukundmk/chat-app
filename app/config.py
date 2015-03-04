6cd0d86946a17556119b40fe858f30a75122563f
mukundmk <mukund.mk93@gmail.com>
2015-03-04 20:42:08 +0530

    test4

--- app/config.py
+++ app/config.py
@@ -11,6 +11,3 @@ database = 'chat_app'
 SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 SQLALCHEMY_DATABASE_URI = 'mysql://' + user_name + ':' + passwd + '@' + host + ':' + port + '/' + database
 
-GOOGLE_LOGIN_CLIENT_ID = '150588492556-gfu3h9qq7076jc6pvbdhkm3bqun23mds.apps.googleusercontent.com'
-GOOGLE_LOGIN_CLIENT_SECRET = 'PHjTGlr_Ql4DV3819GNy5508'
-GOOGLE_LOGIN_REDIRECT_URI = 'http://localhost:5000/oauth2callback'
