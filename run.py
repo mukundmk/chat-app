a9f39c76ab1f8161adb8ebdc66e143e9bc09131f
mukundmk <mukund.mk93@gmail.com>
2015-03-10 19:44:55 +0530

    runs on http only, use nginx to reverse proxy for ssl

--- run.py
+++ run.py
@@ -4,15 +4,5 @@ import ssl
 import sys
 from app import app, socketio
 
-SSL_CERTIFICATE = ''
-SSL_KEY = ''
-
-if len(sys.argv) > 1 and sys.argv[1] == 'ssl':
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    ssl_context.load_cert_chain(SSL_CERTIFICATE, SSL_KEY)
-    app.run(debug=True, ssl_context=ssl_context)
-
-else:
-    #app.run(debug=True)
-    app.debug = True
-    socketio.run(app)
+app.debug = True
+socketio.run(app)
